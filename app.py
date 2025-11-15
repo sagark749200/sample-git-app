bbed0e5fbd776c0ad35c03d6393dd9443d51a936
Sagar kumar <sagar@gmail.com>
2025-11-15 17:55:03 +0530

    replace header image

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ st.title('CampusX')
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image('unknown.jpg')
+    st.image('screenshot1.jpg')
 
 with col2:
     st.write("""As a Data Science YouTuber with over 380K+ subscribers, I am passionate about teaching others how to master Machine Learning, Deep Learning, Natural Language Processing, Computer Vision and Data Analysis. With over 100 repositories on GitHub, I am committed to sharing my knowledge and experience in these areas. My repositories include a wide range of projects, from simple examples to complex applications, all designed to help learners of all levels. With over 6.5k followers on GitHub, I am constantly engaged with the community, sharing insights and responding to questions. If you are interested in Data Science and looking to improve your skills, be sure to check out my repositories and YouTube channel.""")
